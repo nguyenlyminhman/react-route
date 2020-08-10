bc9e2490a55445dc69af81fb864dc508de1935b3
man nguyen <nguyenlyminhman@gmail.com>
2020-08-10 21:56:45 +0700

    Edit somethings

--- src/components/App.js
+++ src/components/App.js
@@ -11,9 +11,9 @@ class App extends Component {
         return (
             <div>
                 <Nav />
-                {/* <News /> */}
+                <News />
                 {/* <Home /> */}
-                <NewsDetails />
+                {/* <NewsDetails /> */}
                 <Footer />
             </div>
         );
--- src/components/Nav.js
+++ src/components/Nav.js
@@ -8,7 +8,7 @@ class Nav extends Component {
         return (
             <nav className="navbar navbar-expand-lg navbar-light fixed-top" id="mainNav">
                 <div className="container">
-                    <a className="navbar-brand js-scroll-trigger" href="#page-top">Start Bootstrap</a>
+                    <a className="navbar-brand js-scroll-trigger" href="#page-top">React Router - News</a>
                     <button className="navbar-toggler navbar-toggler-right" type="button" data-toggle="collapse" data-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
                         Menu
                         <i className="fas fa-bars" />
