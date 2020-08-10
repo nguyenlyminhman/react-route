d3f1af621f4c886df394bb1f3bdfdfbfe1fcbb2f
man nguyen <nguyenlyminhman@gmail.com>
2020-08-10 22:21:18 +0700

    Add router to App component

--- src/components/App.js
+++ src/components/App.js
@@ -5,15 +5,17 @@ import Home from './Home';
 import Footer from './Footer';
 import News from './News'
 import NewsDetails from './NewsDetails'
+import RouterNews from '../router';
 
 class App extends Component {
     render() {
         return (
             <div>
                 <Nav />
-                <News />
+                {/* <News /> */}
                 {/* <Home /> */}
                 {/* <NewsDetails /> */}
+                <RouterNews />
                 <Footer />
             </div>
         );
